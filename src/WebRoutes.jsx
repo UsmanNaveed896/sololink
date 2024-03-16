e2b99c0a96605e21d4504fd0d1125d4a8a1d03be
Sadia Saeed <sadia.software@gmail.com>
2024-03-16 04:50:26 -0700

    routes

--- src/WebRoutes.jsx
+++ src/WebRoutes.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from "./pages/homepage/homepage";
 import MakeMusicLink from './pages/MakeMusicLink'
-
+import Login from './pages/auth/login'
+import SignUp from './pages/auth/signup'
+import ForgetPasssword from './pages/auth/forgetPassword'
 const WebRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route exact path='/' element={<Homepage />} />
                 <Route exact path='/makemusiclink' element={<MakeMusicLink />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route exact path='/signup' element={<SignUp />} />
+                <Route exact path='/forgetpasswors' element={<ForgetPasssword />} />
             </Routes>
         </Router>
     );
